570c07f05e53e7a6ee5beff2ca8038027f7b4d3a
ssteja698 <sai.bejjipuram@whitehatjr.com>
2022-04-16 13:49:05 +0530

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Layout from "@components/_App/Layout";
+import React from "react";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  return (
+    <Layout {...pageProps}>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
