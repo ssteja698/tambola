1d7e1ca10182569a17988aa00b3baeff95908f24
Sai Siva Teja B <sai.bejjipuram@leadsquared.com>
2023-05-02 23:57:50 +0530

    minor

--- src/components/Screens/DesignSystem/index.js
+++ src/components/Screens/DesignSystem/index.js
@@ -1,23 +1,14 @@
 import React from "react";
-import Avatar from "@designSystem/Avatar";
 import Button from "@designSystem/Button";
 import Card from "@designSystem/Card";
 import s from "./index.module.scss";
 
 function DesignSystemScreen() {
-  const avatarProps = {
-    text: "Bro Zoro",
-    size: 80,
-    showSingleLetter: true,
-  };
   const handleButtonClick = () => {
     alert("Button clicked!");
   };
   return (
     <div className={s.designSystemContainer}>
-      <Card title={"Avatar"}>
-        <Avatar {...avatarProps} />
-      </Card>
       <Card title={"Button"}>
         <Button
           primary
